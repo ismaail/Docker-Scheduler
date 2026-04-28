6ecd4393a8602a646af31456a81bdbdef339a23c
ismaail <contact@ismaail.com>
2026-04-28 21:54:07 +0100

    Add logger() with Monolog

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -2,9 +2,54 @@
 
 declare(strict_types=1);
 
+use Monolog\Formatter\JsonFormatter;
+use Monolog\Formatter\LineFormatter;
+use Monolog\Handler\SocketHandler;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+
 if (! function_exists('env')) {
     function env(string $key, mixed $default = null): mixed
     {
         return $_ENV[$key] ?? $default;
+    }
+}
+
+if (! function_exists('logger')) {
+    function logger(): Logger
+    {
+        static $logger = null;
+
+        if (null !== $logger) {
+            return $logger;
+        }
+
+        $logger = new Logger('app');
+
+        $level = Logger::toMonologLevel(env('LOG_LEVEL', 'debug'));
+        $channel = env('MONOLOG_DEFAULT_CHANNEL', 'stdout');
+
+        if ('socket' === $channel) {
+            $socketUrl = env('LOG_SOCKET_URL');
+
+            if (! $socketUrl) {
+                throw new RuntimeException('LOG_SOCKET_URL is not defined for socket channel');
+            }
+
+            $handler = new SocketHandler($socketUrl, $level);
+            $handler->setFormatter(new JsonFormatter());
+        } else {
+            $handler = new StreamHandler('php://stdout', $level);
+            $handler->setFormatter(new LineFormatter(
+                format: "[%datetime%] %level_name%: %message% %context% %extra%\n",
+                dateFormat: 'Y-m-d H:i:s',
+                allowInlineLineBreaks: true,
+                ignoreEmptyContextAndExtra: true,
+            ));
+        }
+
+        $logger->pushHandler($handler);
+
+        return $logger;
     }
 }
