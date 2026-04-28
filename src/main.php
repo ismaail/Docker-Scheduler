07d19bcfca21a54dd0993f671fb2aaa98d30d1de
ismaail <contact@ismaail.com>
2026-04-28 03:49:03 +0100

    Update main.php

--- src/main.php
+++ src/main.php
@@ -4,9 +4,29 @@ declare(strict_types=1);
 
 require __DIR__ . '/../bootstrap/app.php';
 
+use App\Docker\ContainerRepository;
+use App\Docker\LabelParser;
 use Docker\Docker;
 
 $docker = Docker::create();
-$containers = $docker->containerList();
+$repository = new ContainerRepository($docker, new LabelParser());
 
-dump($containers);
+$jobs = $repository->discoverJobs();
+
+if (empty($jobs)) {
+    echo PHP_EOL . 'No jobs found. To enable a container add these labels:' . PHP_EOL;
+    echo '  acme.enabled=true' . PHP_EOL;
+    echo '  acme.<n>.schedule=* * * * *' . PHP_EOL;
+    echo '  acme.<n>.command=php artisan schedule:run' . PHP_EOL;
+
+    exit(0);
+}
+
+foreach ($jobs as $job) {
+    echo PHP_EOL;
+    echo "  Container : {$job->containerName}" . PHP_EOL;
+    echo '  ID        : ' . substr($job->containerId, 0, 12) . PHP_EOL;
+    echo "  Job       : {$job->jobName}" . PHP_EOL;
+    echo "  Schedule  : {$job->schedule}" . PHP_EOL;
+    echo "  Command   : {$job->command}" . PHP_EOL;
+}
